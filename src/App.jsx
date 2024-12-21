990a7a082d4bc76e4a6b8af17ca07366f5129725
Jordy Vuong <joordyr2@gmail.com>
2024-12-22 00:44:08 +0100

    hero + about + button done

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,21 @@
  * Components
  */
 import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
 
 const App = () => {
   
   return (
+    <>
     <Header />
-  )
+    <main>
+      <Hero />
+      <About />
+    </main>
 
+  </>
+  )
 }
 
 
