3c45e8322e8ff65aeb35b0ad0e1bf1212bb5fe03
Jordy Vuong <joordyr2@gmail.com>
2024-12-22 01:26:12 +0100

    navbar, hero, skills,button done

---
+++ src/components/Skill.jsx
@@ -0,0 +1,88 @@
+/**
+ * @copyright 2024 Jordy VUONG
+ * @license Apache-2.0
+ */
+
+
+/**
+ * Components
+ */
+import SkillCard from "./SkillCard";
+
+
+const skillItem = [
+    {
+      imgSrc: '/images/figma.svg',
+      label: 'Figma',
+      desc: 'Design tool'
+    },
+    {
+      imgSrc: '/images/css3.svg',
+      label: 'CSS',
+      desc: 'User Interface'
+    },
+    {
+      imgSrc: '/images/javascript.svg',
+      label: 'JavaScript',
+      desc: 'Interaction'
+    },
+    {
+      imgSrc: '/images/nodejs.svg',
+      label: 'NodeJS',
+      desc: 'Web Server'
+    },
+    {
+      imgSrc: '/images/expressjs.svg',
+      label: 'ExpressJS',
+      desc: 'Node Framework'
+    },
+    {
+      imgSrc: '/images/mongodb.svg',
+      label: 'MongoDB',
+      desc: 'Database'
+    },
+    {
+      imgSrc: '/images/react.svg',
+      label: 'React',
+      desc: 'Framework'
+    },
+    {
+      imgSrc: '/images/tailwindcss.svg',
+      label: 'TailwindCSS',
+      desc: 'User Interface'
+    },
+  ];
+
+
+const Skill = () => {
+  return (
+    <section className="section">
+        <div className="container">
+
+            <h2 className="headline-2">
+                Outils essentiels que j'utilise
+            </h2>
+
+            <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch]">
+                Découvrez les outils et technologies que j'utilise pour créer des sites web et des applications exceptionnels et performants.
+            </p>
+
+            <div className="grid gap-3 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]">
+                {
+                    skillItem.map(({ imgSrc, label, desc }, key)=> (
+                        <SkillCard
+                            key={key}
+                            imgSrc={imgSrc}
+                            label={label}
+                            desc={desc}
+                        />
+                    ))
+                }
+            </div>
+
+        </div>
+    </section>
+  )
+}
+
+export default Skill
